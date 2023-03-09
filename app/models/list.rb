8113648969635f38c0f77201239e78cbe10f90c1
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-09 08:45:00 +0000

    [update]4-１１章

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
   has_one_attached :image
-  
+
   validates :title, presence: true
   validates :body, presence: true
+  # rails cを試すために、以下をコメントアウト(試さないときはコメントアウトを外す)
   validates :image, presence: true
 end
